fixed SPBP for scaling factor 3 and 4

models/modules/SPBP.py
@@ -28,7 +28,7 @@ class CALayer(nn.Module):
 class SubPixelBackProjection(nn.Module):
     def __init__(self, num_features, num_groups, upscale_factor, act_type, norm_type):
         super(SubPixelBackProjection, self).__init__()
-        stride = 2
+        stride = upscale_factor
         padding = 1
         kernel_size = 3
 
@@ -78,6 +78,7 @@ class SubPixelBackProjection(nn.Module):
 
         lr_features = []
         hr_features = []
+        #print(x.shape)
         lr_features.append(x)
 
         for idx in range(self.num_groups):

models/modules/common.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import math
 # https://github.com/thstkdgus35/EDSR-PyTorch/blob/master/src/model/common.py
 
 def default_conv(in_channels, out_channels, kernel_size, bias=True):